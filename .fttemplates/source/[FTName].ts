37fe08e8c8e1dd5b4080723a5b6b0e8b649876d9
Andrey Zaitsev <63799449+Cabagemage@users.noreply.github.com>
2024-05-29 15:06:05 +0300

    chore: Улучшения для темплейтов (ВСкод и Вебшторм) (#13)
    * chore: Улучшения для темплейтов (ВСкод и Вебшторм)
    
    * chore: Улучшения для темплейтов (ВСкод и Вебшторм)

---
+++ .fttemplates/source/[FTName].ts
@@ -0,0 +1,6 @@
+import { apiHttpClient, type HttpServiceResponse } from '@example/shared';
+
+
+export const <FTName | camelcase>NetworkSources = {};
+
+export type <FTName | capitalize>NetworkSources = typeof <FTName | camelcase>NetworkSources
